73631d6c6e2ff7bab74d642e1a5558d43d38211b
r-intmax <144136975+r-intmax@users.noreply.github.com>
2024-04-11 07:25:32 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,11 +1,22 @@
 import os, time, http.server
 
-from flask import Flask, request, abort, jsonify
+from flask import Flask, request, abort, jsonify, make_response
 import argparse
 
 import browser, search
 
 app = Flask(__name__)
+
+mime_types = {
+    '.css': 'text/css',
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.json': 'application/json',
+    '.pdf': 'application/pdf',
+    '.zip': 'application/zip',
+}
+
+def get_mime_type(path): return next((v for k, v in mime_types.items() if k in path), 'text/html')
 
 @app.route('/close', methods=['GET'])
 def close_browser():
@@ -14,7 +25,7 @@ def close_browser():
 
 @app.route('/<path:path>', methods=['GET'])
 def index(path):
-    try: return open(os.path.join('.\\', path.lstrip('/').split('?')[0]), 'rb').read().decode()
+    try: return open(os.path.join('.\\', path.lstrip('/').split('?')[0]), 'rb').read().decode(), 200, {'Content-Type': get_mime_type(path) + '; charset=utf-8'}
     except: abort(404)
 
 @app.route('/search', methods=['GET'])
@@ -29,15 +40,17 @@ def Search():
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument("-b", "--ban", action="store_true", help="Disable local browser")
     parser.add_argument("-c", "--clean", action="store_true", help="Clean Local Temp File")
     parser.add_argument("-d", "--debug", action="store_true", help="Enable debug mode")
     args = parser.parse_args()
     if args.clean:
         os.system('del /s /q "%AppData%\\..\\Local\\Temp\\*.*" &')
     backgrond_browser = browser.Browser(debug=args.debug)
-    client = browser.Browser(debug=True)
-    client.driver.execute_script('window.open("http://localhost:114/search")')
-    client.driver.switch_to.window(client.driver.window_handles[0])
-    client.driver.close()
-    client.driver.switch_to.window(client.driver.window_handles[0])
+    if not args.ban:
+        client = browser.Browser(debug=True)
+        client.driver.execute_script('window.open("http://localhost:114/search")')
+        client.driver.switch_to.window(client.driver.window_handles[0])
+        client.driver.close()
+        client.driver.switch_to.window(client.driver.window_handles[0])
     app.run(host='localhost', port=114)
